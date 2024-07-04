b2083e6b993e0894a55692b353b1af8bcf0ce0ca
MikeyTheA <170099338+MikeyTheA@users.noreply.github.com>
2024-07-04 14:58:20 +0300

    crypto in sandbox for uuid gen

--- src/modloader/src/sandbox.ts
+++ src/modloader/src/sandbox.ts
@@ -1,32 +1,32 @@
 export class Sandbox {
-  public iframe: HTMLIFrameElement;
-  public sandboxWindow: any;
-  public env: any;
+	public iframe: HTMLIFrameElement;
+	public sandboxWindow: any;
+	public env: any;
 
-  constructor(env: any = {}) {
-    this.iframe = document.createElement("iframe");
-    //this.iframe.style.display = "none";
-    this.iframe.style.width = "100%";
-    this.iframe.style.height = "100%";
-    this.iframe.style.position = "fixed";
-    this.iframe.style.left = "0px";
-    this.iframe.style.top = "0px";
-    this.iframe.style.border = "none";
-    this.iframe.style.backgroundColor = "transparent";
-    this.iframe.style.background = "transparent";
-    this.iframe.style.pointerEvents = "none";
-    this.iframe.style.zIndex = "1";
-    this.env = env;
-    document.body.appendChild(this.iframe);
+	constructor(env: any = {}) {
+		this.iframe = document.createElement('iframe');
+		//this.iframe.style.display = "none";
+		this.iframe.style.width = '100%';
+		this.iframe.style.height = '100%';
+		this.iframe.style.position = 'fixed';
+		this.iframe.style.left = '0px';
+		this.iframe.style.top = '0px';
+		this.iframe.style.border = 'none';
+		this.iframe.style.backgroundColor = 'transparent';
+		this.iframe.style.background = 'transparent';
+		this.iframe.style.pointerEvents = 'none';
+		this.iframe.style.zIndex = '1';
+		this.env = env;
+		document.body.appendChild(this.iframe);
 
-    this.sandboxWindow = this.iframe.contentWindow;
+		this.sandboxWindow = this.iframe.contentWindow;
 
-    this.clearEnv();
-    this.refreshEnv(env);
-  }
+		this.clearEnv();
+		this.refreshEnv(env);
+	}
 
-  clearEnv() {
-    this.sandboxWindow.document.documentElement.innerHTML = `
+	clearEnv() {
+		this.sandboxWindow.document.documentElement.innerHTML = `
       <html>
         <head>
           <style>
@@ -37,78 +37,79 @@ export class Sandbox {
       </html>
     `;
 
-    const whitelisted = [
-      0,             // [Things needed for basic functionality]
-      "location",    //
-      "Object",      //
-      "String",      //
-      "Number",      //
-      "Bigint",      //
-      "boolean",     //
-      "Undefined",   //
-      "Null",        //
-      "Symbol",      //
-      "Math",        //
-      "NaN",         //
-      "Iterator",    //
-      "isFinite",    //
-      "isNaN",       //
-      "JSON",        //
-      "Map",         //
-      "Infinity",    //
-      "Date",        //
-      "BigInt",      //
-      "Array",       //
-      "Function",    //
-      "parseFloat",  //
-      "parseInt",    //
-      "Promise",     //
-      "Set",         //
-      "undefined",   //
-      "document",    // [Things needed for html functionality]
-      "Element",     //
-      "HTMLElement", //
-      "WebSocket",   // [WebSocket access for things like OBS overlays and such]
-      "fetch",       // [Web access, for accessing things like wikis, bulbapedia etc]
-      "eval",        // [Modception!!]
-    ];
-    Object.getOwnPropertyNames(this.sandboxWindow).forEach((key) => {
-      if (!whitelisted.includes(key)) {
-        try {
-          this.sandboxWindow[key] = undefined;
-        } catch { }
-        try {
-          delete this.sandboxWindow[key];
-        } catch { }
-      }
-    });
-  }
+		const whitelisted = [
+			0, // [Things needed for basic functionality]
+			'location', //
+			'Object', //
+			'String', //
+			'Number', //
+			'Bigint', //
+			'boolean', //
+			'Undefined', //
+			'Null', //
+			'Symbol', //
+			'Math', //
+			'NaN', //
+			'Iterator', //
+			'isFinite', //
+			'isNaN', //
+			'JSON', //
+			'Map', //
+			'Infinity', //
+			'Date', //
+			'BigInt', //
+			'Array', //
+			'Function', //
+			'parseFloat', //
+			'parseInt', //
+			'Promise', //
+			'Set', //
+			'undefined', //
+			'document', // [Things needed for html functionality]
+			'Element', //
+			'HTMLElement', //
+			'WebSocket', // [WebSocket access for things like OBS overlays and such]
+			'fetch', // [Web access, for accessing things like wikis, bulbapedia etc]
+			'eval', // [Modception!!]
+			'crypto', // [UUID generation]
+		];
+		Object.getOwnPropertyNames(this.sandboxWindow).forEach((key) => {
+			if (!whitelisted.includes(key)) {
+				try {
+					this.sandboxWindow[key] = undefined;
+				} catch {}
+				try {
+					delete this.sandboxWindow[key];
+				} catch {}
+			}
+		});
+	}
 
-  refreshEnv(env: any = this.env) {
-    for (const [key, value] of Object.entries(env)) {
-      this.sandboxWindow[key] = value;
-    }
-  }
+	refreshEnv(env: any = this.env) {
+		for (const [key, value] of Object.entries(env)) {
+			this.sandboxWindow[key] = value;
+		}
+	}
 
-  eval(code: string) {
-    try {
-      const script = this.sandboxWindow.document.createElement("script");
-      script.textContent = `
+	eval(code: string) {
+		try {
+			const script = this.sandboxWindow.document.createElement('script');
+			script.textContent = `
                 try{
                     ${code}
                 }catch(e){
                     error(e.message)
                 }
             `;
-      this.sandboxWindow.document.body.appendChild(script);
-    } catch (e) {
-      const errorScript = this.sandboxWindow.document.createElement("script");
-      errorScript.textContent = "error(e.message)";
-      this.sandboxWindow.document.body.appendChild(errorScript);
-    }
-  }
+			this.sandboxWindow.document.body.appendChild(script);
+		} catch (e) {
+			const errorScript = this.sandboxWindow.document.createElement('script');
+			errorScript.textContent = 'error(e.message)';
+			this.sandboxWindow.document.body.appendChild(errorScript);
+		}
+	}
 
-  destroy() {
-    document.body.removeChild(this.iframe);
-  }
+	destroy() {
+		document.body.removeChild(this.iframe);
+	}
 }
