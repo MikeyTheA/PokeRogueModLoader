aaf7047c107dd1892ca8ea56d2cd94198278e79f
MikeyTheA <170099338+MikeyTheA@users.noreply.github.com>
2024-07-04 14:58:34 +0300

    window errors also go to logs now

--- src/modloader/src/mod.ts
+++ src/modloader/src/mod.ts
@@ -1,235 +1,239 @@
-import LZstring from "../lib/lz-string.js";
+import LZstring from '../lib/lz-string.js';
 
-import StaticManager from "./data";
-import { LoaderData } from "./main";
-import { Sandbox } from "./sandbox";
-import { WindowFlags, windowHandler } from "./windows";
+import StaticManager from './data';
+import { LoaderData } from './main';
+import { Sandbox } from './sandbox';
+import { WindowFlags, windowHandler } from './windows';
 
-import PokeRogue from "./all-modules";
-window["PokeRogue"] = PokeRogue;
+import PokeRogue from './all-modules';
+window['PokeRogue'] = PokeRogue;
 export type ModData = {
-  id?: string;
-  name?: string;
-  description?: string;
-  author?: string;
-  version?: number;
-  scripts?: Array<ScriptData>;
+	id?: string;
+	name?: string;
+	description?: string;
+	author?: string;
+	version?: number;
+	scripts?: Array<ScriptData>;
 };
 
 export type ScriptData = {
-  id?: string;
-  name?: string;
-  code?: string;
+	id?: string;
+	name?: string;
+	code?: string;
 };
 
 export type Hook = {
-  phase: string;
-  func: Function;
+	phase: string;
+	func: Function;
 };
 
 export class ModsHandler {
-  public mods: Array<Mod>;
+	public mods: Array<Mod>;
 
-  constructor() {
-    this.mods = [];
-  }
+	constructor() {
+		this.mods = [];
+	}
 
-  addMod(data: ModData = {}) {
-    const mod = new Mod(data, this);
-    this.mods.push(mod);
-    this.save();
-  }
+	addMod(data: ModData = {}) {
+		const mod = new Mod(data, this);
+		this.mods.push(mod);
+		this.save();
+	}
 
-  deleteMod(id: string) {
-    const mod = this.getMod(id);
-    if (mod) {
-      mod.delete();
-      return true;
-    } else {
-      return false;
-    }
-  }
+	deleteMod(id: string) {
+		const mod = this.getMod(id);
+		if (mod) {
+			mod.delete();
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  getMod(id: string) {
-    return this.mods.find((mod) => mod.id === id);
-  }
+	getMod(id: string) {
+		return this.mods.find((mod) => mod.id === id);
+	}
 
-  save() {
-    const mods = this.compress();
-    localStorage.setItem("MokeRogueMods", LZstring.compressToUTF16(JSON.stringify(mods)));
-    return true;
-  }
+	save() {
+		const mods = this.compress();
+		localStorage.setItem('MokeRogueMods', LZstring.compressToUTF16(JSON.stringify(mods)));
+		return true;
+	}
 
-  compress() {
-    const mods: Array<ModData> = this.mods.map((mod) => ({
-      id: mod.id,
-      name: mod.name,
-      description: mod.description,
-      author: mod.author,
-      version: mod.version,
-      scripts: mod.scripts.map((script) => ({
-        id: script.id,
-        name: script.name,
-        code: script.code,
-      })),
-    }));
+	compress() {
+		const mods: Array<ModData> = this.mods.map((mod) => ({
+			id: mod.id,
+			name: mod.name,
+			description: mod.description,
+			author: mod.author,
+			version: mod.version,
+			scripts: mod.scripts.map((script) => ({
+				id: script.id,
+				name: script.name,
+				code: script.code,
+			})),
+		}));
 
-    return mods;
-  }
+		return mods;
+	}
 
-  load() {
-    const mods = LZstring.decompressFromUTF16(localStorage.getItem("MokeRogueMods"));
-    if (mods) {
-      const parsedMods: Array<ModData> = JSON.parse(mods);
-      parsedMods.map((modData) => this.addMod(modData));
-    }
-  }
+	load() {
+		const mods = LZstring.decompressFromUTF16(localStorage.getItem('MokeRogueMods'));
+		if (mods) {
+			const parsedMods: Array<ModData> = JSON.parse(mods);
+			parsedMods.map((modData) => this.addMod(modData));
+		}
+	}
 }
 
 class Mod {
-  public id: string;
-  public name: string;
-  public description: string;
-  public author: string;
-  public version: number;
-  public scripts: Array<Script>;
-  public modsHandler: ModsHandler;
-  public data: StaticManager;
+	public id: string;
+	public name: string;
+	public description: string;
+	public author: string;
+	public version: number;
+	public scripts: Array<Script>;
+	public modsHandler: ModsHandler;
+	public data: StaticManager;
 
-  public github: boolean;
+	public github: boolean;
 
-  constructor(data: ModData, modsHandler: ModsHandler) {
-    this.id = data.id || crypto.randomUUID();
-    this.name = data.name || "Unnamed mod";
-    this.description = data.description || "";
-    this.author = data.author || "Unknown author";
-    this.version = data.version || 1;
-    this.modsHandler = modsHandler;
+	constructor(data: ModData, modsHandler: ModsHandler) {
+		this.id = data.id || crypto.randomUUID();
+		this.name = data.name || 'Unnamed mod';
+		this.description = data.description || '';
+		this.author = data.author || 'Unknown author';
+		this.version = data.version || 1;
+		this.modsHandler = modsHandler;
 
-    this.data = new StaticManager(this.id);
+		this.data = new StaticManager(this.id);
 
-    this.github = this.id.startsWith("https://github.com/");
-    this.scripts = (data.scripts || []).map((scriptData) => new Script(scriptData, this));
-  }
+		this.github = this.id.startsWith('https://github.com/');
+		this.scripts = (data.scripts || []).map((scriptData) => new Script(scriptData, this));
+	}
 
-  delete() {
-    this.scripts.forEach((script) => script.delete());
-    this.modsHandler.mods = this.modsHandler.mods.filter((mod) => mod.id !== this.id);
-    this.modsHandler.save();
-  }
+	delete() {
+		this.scripts.forEach((script) => script.delete());
+		this.modsHandler.mods = this.modsHandler.mods.filter((mod) => mod.id !== this.id);
+		this.modsHandler.save();
+	}
 
-  addScript(data: ScriptData) {
-    const script = new Script(data, this);
-    this.scripts.push(script);
-    this.modsHandler.save();
-  }
+	addScript(data: ScriptData) {
+		const script = new Script(data, this);
+		this.scripts.push(script);
+		this.modsHandler.save();
+	}
 
-  deleteScript(id: string) {
-    this.scripts.find((script) => script.id !== id).delete();
-  }
+	deleteScript(id: string) {
+		this.scripts.find((script) => script.id !== id).delete();
+	}
 }
 
 class Script {
-  public id: string;
-  public name: string;
-  private _code: string;
-  public hooks: Array<Hook>;
-  public mod: Mod;
-  public sandbox: Sandbox;
+	public id: string;
+	public name: string;
+	private _code: string;
+	public hooks: Array<Hook>;
+	public mod: Mod;
+	public sandbox: Sandbox;
 
-  constructor(data: ScriptData, mod: Mod) {
-    this.id = data.id || crypto.randomUUID();
-    this.name = data.name || "Unnamed script";
-    this.hooks = [];
-    this.mod = mod;
+	constructor(data: ScriptData, mod: Mod) {
+		this.id = data.id || crypto.randomUUID();
+		this.name = data.name || 'Unnamed script';
+		this.hooks = [];
+		this.mod = mod;
 
-    this.sandbox = new Sandbox({
-      ImGui: ImGui,
-      data: this.mod.data,
-      globalData: new StaticManager("global"),
-      log: (message: string) => {
-        const logs = LoaderData.getData(`LogsForMod${this.mod.id}`, [], false);
-        logs.push(["log", message]);
-        LoaderData.setData(`LogsForMod${this.mod.id}`, logs);
-      },
-      error: (message: string) => {
-        const logs = LoaderData.getData(`LogsForMod${this.mod.id}`, [], false);
-        logs.push(["error", message]);
-        LoaderData.setData(`LogsForMod${this.mod.id}`, logs);
-      },
-      getBattleScene: () => {
-        if (window.Phaser && Phaser.Display && Phaser.Display.Canvas && Phaser.Display.Canvas.CanvasPool && (Phaser.Display.Canvas.CanvasPool as any).pool[1] && (Phaser.Display.Canvas.CanvasPool as any).pool[1].parent && (Phaser.Display.Canvas.CanvasPool as any).pool[1].parent.scene) {
-          return (Phaser.Display.Canvas.CanvasPool as any).pool[1].parent.scene;
-        }
-        return false;
-      },
-      addWindow: (name: string, show: Function, flags: WindowFlags) => {
-        windowHandler.addWindow(name, show, flags, `${this.id}|${name}`);
-      },
-      hook: (phase: string, func: Function) => {
-        this.hooks.push({ phase: phase, func: func });
-      },
-      getInstalledMods: () => {
-        return this.mod.modsHandler.mods.map((mod) => ({
-          name: mod.name,
-          version: mod.version,
-          author: mod.author,
-        }));
-      },
-      PokeRogue: PokeRogue,
-      Phaser: Phaser,
-      getWindowOpenAccess: (windowName: string) => {
-        const window = windowHandler.Windows.find(window => window.name === windowName && window.identifier === `${this.id}|${windowName}`)
-        if (window) {
-          return LoaderData.getAccess(`WindowOpenState${this.id}|${windowName}`, window.flags.open, window.flags.persistentOpen)
-        }
-        return false
-      },
-      getGameCanvasSize: () => {
-        const canvasRect = document.getElementById("app").getElementsByTagName("canvas")[0].getBoundingClientRect();
-        return [canvasRect.left, canvasRect.top]
-      }
-    });
+		this.sandbox = new Sandbox({
+			ImGui: ImGui,
+			data: this.mod.data,
+			globalData: new StaticManager('global'),
+			log: (message: string) => {
+				const logs = LoaderData.getData(`LogsForMod${this.mod.id}`, [], false);
+				logs.push(['log', message]);
+				LoaderData.setData(`LogsForMod${this.mod.id}`, logs);
+			},
+			error: (message: string) => {
+				const logs = LoaderData.getData(`LogsForMod${this.mod.id}`, [], false);
+				logs.push(['error', message]);
+				LoaderData.setData(`LogsForMod${this.mod.id}`, logs);
+			},
+			getBattleScene: () => {
+				if (window.Phaser && Phaser.Display && Phaser.Display.Canvas && Phaser.Display.Canvas.CanvasPool && (Phaser.Display.Canvas.CanvasPool as any).pool[1] && (Phaser.Display.Canvas.CanvasPool as any).pool[1].parent && (Phaser.Display.Canvas.CanvasPool as any).pool[1].parent.scene) {
+					return (Phaser.Display.Canvas.CanvasPool as any).pool[1].parent.scene;
+				}
+				return false;
+			},
+			addWindow: (name: string, show: Function, flags: WindowFlags) => {
+				windowHandler.addWindow(name, show, flags, `${this.id}|${name}`, (message: string) => {
+					const logs = LoaderData.getData(`LogsForMod${this.mod.id}`, [], false);
+					logs.push(['error', message]);
+					LoaderData.setData(`LogsForMod${this.mod.id}`, logs);
+				});
+			},
+			hook: (phase: string, func: Function) => {
+				this.hooks.push({ phase: phase, func: func });
+			},
+			getInstalledMods: () => {
+				return this.mod.modsHandler.mods.map((mod) => ({
+					name: mod.name,
+					version: mod.version,
+					author: mod.author,
+				}));
+			},
+			PokeRogue: PokeRogue,
+			Phaser: Phaser,
+			getWindowOpenAccess: (windowName: string) => {
+				const window = windowHandler.Windows.find((window) => window.name === windowName && window.identifier === `${this.id}|${windowName}`);
+				if (window) {
+					return LoaderData.getAccess(`WindowOpenState${this.id}|${windowName}`, window.flags.open, window.flags.persistentOpen);
+				}
+				return false;
+			},
+			getGameCanvasSize: () => {
+				const canvasRect = document.getElementById('app').getElementsByTagName('canvas')[0].getBoundingClientRect();
+				return [canvasRect.left, canvasRect.top];
+			},
+		});
 
-    this.code = data.code || "print(\"Hello world!\")";
-  }
+		this.code = data.code || 'print("Hello world!")';
+	}
 
-  reload() {
-    this.clean();
-    this.sandbox.clearEnv();
-    this.sandbox.refreshEnv();
-    this.sandbox.eval(this.code);
-  }
+	reload() {
+		this.clean();
+		this.sandbox.clearEnv();
+		this.sandbox.refreshEnv();
+		this.sandbox.eval(this.code);
+	}
 
-  clean() {
-    if (this.sandbox.sandboxWindow.cleanup) {
-      this.sandbox.sandboxWindow.cleanup();
-    }
+	clean() {
+		if (this.sandbox.sandboxWindow.cleanup) {
+			this.sandbox.sandboxWindow.cleanup();
+		}
 
-    this.sandbox.sandboxWindow.cleanup = undefined;
-    this.sandbox.sandboxWindow.update = undefined;
-    this.sandbox.sandboxWindow.onPhasePush = undefined;
+		this.sandbox.sandboxWindow.cleanup = undefined;
+		this.sandbox.sandboxWindow.update = undefined;
+		this.sandbox.sandboxWindow.onPhasePush = undefined;
 
-    this.hooks = [];
+		this.hooks = [];
 
-    windowHandler.Windows = windowHandler.Windows.filter((window) => !window.identifier.startsWith(this.id as string));
+		windowHandler.Windows = windowHandler.Windows.filter((window) => !window.identifier.startsWith(this.id as string));
 
-    this.mod.data.cleanListeners(this.id);
-  }
+		this.mod.data.cleanListeners(this.id);
+	}
 
-  delete() {
-    this.clean();
-    this.mod.scripts = this.mod.scripts.filter((script) => script.id !== this.id);
-    this.mod.modsHandler.save();
-  }
+	delete() {
+		this.clean();
+		this.mod.scripts = this.mod.scripts.filter((script) => script.id !== this.id);
+		this.mod.modsHandler.save();
+	}
 
-  set code(newCode: string) {
-    this._code = newCode;
-    LoaderData.setData(`JSBeautified|${this.id}`, undefined, false);
-    this.reload();
-  }
+	set code(newCode: string) {
+		this._code = newCode;
+		LoaderData.setData(`JSBeautified|${this.id}`, undefined, false);
+		this.reload();
+	}
 
-  get code() {
-    return this._code;
-  }
+	get code() {
+		return this._code;
+	}
 }
--- src/modloader/src/windows.ts
+++ src/modloader/src/windows.ts
@@ -1,62 +1,65 @@
-import { LoaderData } from "./main";
+import { LoaderData } from './main';
 
 export type Window = {
-  identifier: string;
-  name: string;
-  show: Function;
-  flags: WindowFlags;
+	identifier: string;
+	name: string;
+	show: Function;
+	flags: WindowFlags;
+	errorFunc: (message: string) => void;
 };
 
 export type WindowFlags = {
-  open?: boolean;
-  persistentOpen?: boolean;
-  hidden?: boolean;
-  noClose?: boolean;
-  initialWidth?: number;
-  initialHeight?: number;
-  scriptId?: number;
+	open?: boolean;
+	persistentOpen?: boolean;
+	hidden?: boolean;
+	noClose?: boolean;
+	initialWidth?: number;
+	initialHeight?: number;
+	scriptId?: number;
 };
 
 export class WindowHandler {
-  public Windows: Array<Window>;
+	public Windows: Array<Window>;
 
-  constructor() {
-    this.Windows = [];
-  }
+	constructor() {
+		this.Windows = [];
+	}
 
-  addWindow(name: string, show: Function, flags: WindowFlags, identifier: string = crypto.randomUUID()) {
-    const window: Window = {
-      identifier: identifier || crypto.randomUUID(),
-      name: name,
-      show: show,
-      flags: {
-        open: flags.open ?? false,
-        persistentOpen: flags.persistentOpen ?? true,
-        hidden: flags.hidden ?? false,
-        noClose: flags.noClose ?? false,
-        initialWidth: flags.initialWidth ?? 500,
-        initialHeight: flags.initialHeight ?? 440,
-      },
-    };
-    this.Windows.push(window);
-  }
+	addWindow(name: string, show: Function, flags: WindowFlags, identifier: string = crypto.randomUUID(), errorFunc = (message: string) => {}) {
+		const window: Window = {
+			identifier: identifier || crypto.randomUUID(),
+			name: name,
+			show: show,
+			errorFunc: errorFunc,
+			flags: {
+				open: flags.open ?? false,
+				persistentOpen: flags.persistentOpen ?? true,
+				hidden: flags.hidden ?? false,
+				noClose: flags.noClose ?? false,
+				initialWidth: flags.initialWidth ?? 500,
+				initialHeight: flags.initialHeight ?? 440,
+			},
+		};
+		this.Windows.push(window);
+	}
 
-  renderWindows() {
-    this.Windows.forEach((window: Window) => {
-      ImGui.SetNextWindowSize(new ImGui.Vec2(window.flags.initialWidth, window.flags.initialHeight), ImGui.Cond.FirstUseEver);
-      const windowOpen: (value?: boolean) => boolean = LoaderData.getAccess(`WindowOpenState${window.identifier}`, window.flags.open, window.flags.persistentOpen);
-      if ((windowOpen() as boolean) && ImGui.Begin(window.name, (!window.flags.noClose && windowOpen) || undefined)) {
-        try {
-          window.show();
-        } catch (e) {
-          ImGui.TextColored(new ImGui.ImVec4(1.0, 0.0, 0.0, 1.0), "error: ");
-          ImGui.SameLine();
-          ImGui.Text(e.message);
-        }
-        ImGui.End();
-      }
-    });
-  }
+	renderWindows() {
+		this.Windows.forEach((window: Window) => {
+			ImGui.SetNextWindowSize(new ImGui.Vec2(window.flags.initialWidth, window.flags.initialHeight), ImGui.Cond.FirstUseEver);
+			const windowOpen: (value?: boolean) => boolean = LoaderData.getAccess(`WindowOpenState${window.identifier}`, window.flags.open, window.flags.persistentOpen);
+			if ((windowOpen() as boolean) && ImGui.Begin(window.name, (!window.flags.noClose && windowOpen) || undefined)) {
+				try {
+					window.show();
+				} catch (e) {
+					ImGui.TextColored(new ImGui.ImVec4(1.0, 0.0, 0.0, 1.0), 'error: ');
+					ImGui.SameLine();
+					ImGui.Text(e.message);
+					window.errorFunc(e.message);
+				}
+				ImGui.End();
+			}
+		});
+	}
 }
 
 export const windowHandler = new WindowHandler();
